6ed260b3b3ca10602c8e08643fbd865990d811be
keichan <th15154p3n@gmail.com>
2024-07-06 20:04:44 +0000

    updated versions of terraform providers

--- terraform/00-providers.tf
+++ terraform/00-providers.tf
@@ -1,29 +1,26 @@
 locals {
   kubernetes_version = "v1.29.2"
 }
+
 terraform {
   required_version = ">=1.8.0"
 
   required_providers {
-    # kind = {
-    #   source = "tehcyx/kind"
-    #   version = "~>0.4.0"
-    # }
     minikube = {
       source = "scott-the-programmer/minikube"
       version = "~>0.3.10"
     }
     grafana = {
       source = "grafana/grafana"
-      version = "~>2.18.0"
+      version = "~>3.2.1"
     }
     helm = {
       source = "hashicorp/helm"
-      version = "~>2.13.1"
+      version = "~>2.14.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~>2.29.0"
+      version = "~>2.31.0"
     }
   }
 }
