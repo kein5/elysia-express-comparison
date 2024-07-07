d6545ea69077583a8e56c26a2468abc5dfc3c37c
keichan <th15154p3n@gmail.com>
2024-07-07 09:01:14 +0000

    updated terraform: manifest files related to monitoring

--- terraform/03-prometheus.tf
+++ terraform/03-prometheus.tf
@@ -5,18 +5,23 @@ data "external" "monitoring_yaml_files" {
     "${path.module}/scripts/get_yaml_files.sh",
     "${path.module}/../monitoring"
     ]
-
 }
 
 locals {
   yaml_files = data.external.monitoring_yaml_files.result # returns a list of file paths
-  manifest_string = tostring(join("\n", [for f in local.yaml_files: file(f)])) # returns a string of all yaml files
 }
 
 resource "kubernetes_manifest" "monitoring" {
+  # cadvisor for monitoring node-level metrics.
+  # grafana for hosting monitoring UI which receives metrics from prometheus.
+  # kube-state-metrics for observing kubernetes objects.
+  # prometheus for collecting various metrics.
+  # prometheus-operator for automating management of prometheus.
   depends_on = [
+    minikube_cluster.docker,
     helm_release.prometheus_operator_crds,
   ]
+
   for_each = local.yaml_files
   manifest = provider::kubernetes::manifest_decode(file(each.value))
 }
